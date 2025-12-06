43cf5bcb8e4ff982db28347d400f25a67793f4ab
danilauri-024 <rainthora@gmail.com>
2025-12-07 07:50:24 +0800

    added the carousel slideshow

---
+++ js/scrip.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+let slideInterval;
+
+function startSlideshow(){
+    clearInterval(slideInterval);
+
+    slideInterval = setInterval (showSlides, 4000);
+}
+
+function showSlides(){
+    let slides = document.getElementsByClassName("slide-fade");
+
+    for(let i = 0; i< slides.length; i++){
+        slides [i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if(slideIndex > slides.length){
+        slideIndex = 1;
+
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
+
+startSlideshow();
